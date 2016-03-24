7e93cb105a35e80b7d1abc60bc3431f958e597cf
wangjiangchuan <wangjiangchuan@meizu.com>
2016-03-24 17:49:56 +0800

    提交工程

---
+++ app/src/main/java/com/example/root/screenrecorder/activity/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.root.screenrecorder.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.root.screenrecorder.R;
+import com.example.root.screenrecorder.tools.ScreenRecorder;
+
+import java.io.File;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private static final int REQUEST_CODE = 1;
+    private MediaProjectionManager mMediaProjectionManager;
+    private ScreenRecorder mRecorder;
+    private Button mButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(this);
+        //noinspection ResourceType
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(MEDIA_PROJECTION_SERVICE);
+    }
+
+
+    //startActivityResult的返回值
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //获取被捕获的屏幕信息
+        MediaProjection mediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+        if (mediaProjection == null) {
+            Log.e("@@", "media projection is null");
+            return;
+        }
+        // video size
+        final int width = 1080;
+        final int height = 1920;
+        File file = new File(Environment.getExternalStorageDirectory(),
+                "record-" + width + "x" + height + "-" + System.currentTimeMillis() + ".mp4");
+        final int bitrate = 4194304;    //4M
+        Log.e("path", String.valueOf(file.getAbsolutePath()));
+        mRecorder = new ScreenRecorder(width, height, bitrate, 1, mediaProjection, file.getAbsolutePath());
+        mRecorder.start();
+        mButton.setText("Stop Recorder");
+        Toast.makeText(this, "Screen recorder is running...", Toast.LENGTH_SHORT).show();
+        moveTaskToBack(true);
+    }
+
+
+    //单击按钮开始录制屏幕，
+    @Override
+    public void onClick(View v) {
+        if (mRecorder != null) {
+            mRecorder.quit();
+            mRecorder = null;
+            mButton.setText("Restart recorder");
+        } else {
+            Intent captureIntent = mMediaProjectionManager.createScreenCaptureIntent();
+            //此处会有一个询问永华是否开始的对话框
+            startActivityForResult(captureIntent, REQUEST_CODE);
+        }
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mRecorder != null){
+            mRecorder.quit();
+            mRecorder = null;
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/root/screenrecorder/tools/ScreenRecorder.java
@@ -0,0 +1,216 @@
+package com.example.root.screenrecorder.tools;
+
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaCodec;
+import android.media.MediaCodecInfo;
+import android.media.MediaFormat;
+import android.media.MediaMuxer;
+import android.media.projection.MediaProjection;
+import android.util.Log;
+import android.view.Surface;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Created by root on 16-3-23.
+ */
+
+//用来制作视频的工具
+public class ScreenRecorder extends Thread {
+    private static final String TAG = "ScreenRecorder";
+
+    //录制的视频的宽
+    private int mWidth;
+    //录制视频的高
+    private int mHeight;
+    //录制视频的比特率
+    private int mBitRate;
+    private int mDpi;
+    //目标文件路径
+    private String mDstPath;
+    //截屏工具
+    private MediaProjection mMediaProjection;
+    // parameters for the encoder
+    private static final String MIME_TYPE = "video/avc"; // H.264 Advanced Video Coding
+    private static final int FRAME_RATE = 100;   // 30 fps
+    private static final int IFRAME_INTERVAL = 1; // 10 seconds between I-frames
+    private static final int TIMEOUT_US = 1000;
+
+    private MediaCodec mEncoder;
+    private Surface mSurface;
+    private MediaMuxer mMuxer;
+    private boolean mMuxerStarted = false;
+    private int mVideoTrackIndex = -1;
+    private AtomicBoolean mQuit = new AtomicBoolean(false);
+    private MediaCodec.BufferInfo mBufferInfo = new MediaCodec.BufferInfo();
+    //虚拟屏幕（Virtual Display）
+    private VirtualDisplay mVirtualDisplay;
+
+    public ScreenRecorder(int width, int height, int bitrate, int dpi, MediaProjection mp, String dstPath) {
+        super(TAG);
+        mWidth = width;
+        mHeight = height;
+        mBitRate = bitrate;
+        mDpi = dpi;
+        mMediaProjection = mp;
+        mDstPath = dstPath;
+    }
+
+
+    public ScreenRecorder(MediaProjection mp) {
+        // 480p 2Mbps
+        this(640, 480, 2000000, 1, mp, "/sdcard/test.mp4");
+    }
+
+    /**
+     * stop task
+     */
+    public final void quit() {
+        mQuit.set(true);
+    }
+
+    @Override
+    public void run() {
+        try {
+            try {
+                prepareEncoder();
+                //MediaMuxer类主要用于将音频和视频数据进行混合生成多媒体文件
+                //这里是指定路径和输出格式
+                mMuxer = new MediaMuxer(mDstPath, MediaMuxer.OutputFormat.MUXER_OUTPUT_MPEG_4);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //创建虚拟屏幕，将虚拟屏幕投射到没Surface上面，
+            mVirtualDisplay = mMediaProjection.createVirtualDisplay(TAG + "-display",
+                    mWidth, mHeight, mDpi, DisplayManager.VIRTUAL_DISPLAY_FLAG_PUBLIC,
+                    mSurface, null, null);
+            Log.d(TAG, "created virtual display: " + mVirtualDisplay);
+            recordVirtualDisplay();
+
+        } finally {
+            release();
+        }
+    }
+
+    //配置编码器
+    private void prepareEncoder() throws IOException {
+        //媒体文件格式
+        MediaFormat format = MediaFormat.createVideoFormat(MIME_TYPE, mWidth, mHeight);
+        format.setInteger(MediaFormat.KEY_COLOR_FORMAT,
+                MediaCodecInfo.CodecCapabilities.COLOR_FormatSurface);
+        //设置比特率
+        format.setInteger(MediaFormat.KEY_BIT_RATE, mBitRate);
+        //设置帧率
+        format.setInteger(MediaFormat.KEY_FRAME_RATE, FRAME_RATE);
+        //???
+        format.setInteger(MediaFormat.KEY_I_FRAME_INTERVAL, IFRAME_INTERVAL);
+
+        Log.d(TAG, "created video format: " + format);
+        //String MIME_TYPE = "video/avc";
+        mEncoder = MediaCodec.createEncoderByType(MIME_TYPE);
+        //配置MediaCoder
+        mEncoder.configure(format, null, null, MediaCodec.CONFIGURE_FLAG_ENCODE);
+        //设置编码器的输入源
+        mSurface = mEncoder.createInputSurface();
+        Log.d(TAG, "created input surface: " + mSurface);
+        mEncoder.start();
+    }
+
+    private void recordVirtualDisplay() {
+        //mQuit为原子型boolean数据，处理多线程
+        while (!mQuit.get()) {
+            //准备好进行编码或者解码的时，调用dequeueInputBuffer()方法来获得这个用来作为媒体文件源码的ByteBuffer的索引位置
+            int index = mEncoder.dequeueOutputBuffer(mBufferInfo, TIMEOUT_US);
+            Log.i(TAG, "dequeue output buffer index=" + index);
+            //输出格式改变
+            if (index == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                //重置输出格式
+                resetOutputFormat();
+
+            } else if (index == MediaCodec.INFO_TRY_AGAIN_LATER) {
+                Log.d(TAG, "retrieving buffers time out!");
+                try {
+                    // wait 10ms
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                }
+            } else if (index >= 0) {
+                //获取值正确，检查mMuxer是否已经开始了
+                if (!mMuxerStarted) {
+                    throw new IllegalStateException("MediaMuxer dose not call addTrack(format) ");
+                }
+                encodeToVideoTrack(index);
+
+                mEncoder.releaseOutputBuffer(index, false);
+            }
+        }
+    }
+
+    private void encodeToVideoTrack(int index) {
+        ByteBuffer encodedData = mEncoder.getOutputBuffer(index);
+
+        if ((mBufferInfo.flags & MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
+            // The codec config data was pulled out and fed to the muxer when we got
+            // the INFO_OUTPUT_FORMAT_CHANGED status.
+            // Ignore it.
+            Log.d(TAG, "ignoring BUFFER_FLAG_CODEC_CONFIG");
+            mBufferInfo.size = 0;
+        }
+        if (mBufferInfo.size == 0) {
+            Log.d(TAG, "info.size == 0, drop it.");
+            encodedData = null;
+        } else {
+            Log.d(TAG, "got buffer, info: size=" + mBufferInfo.size
+                    + ", presentationTimeUs=" + mBufferInfo.presentationTimeUs
+                    + ", offset=" + mBufferInfo.offset);
+        }
+        if (encodedData != null) {
+            encodedData.position(mBufferInfo.offset);
+            encodedData.limit(mBufferInfo.offset + mBufferInfo.size);
+            //像目标文件写入数据
+            mMuxer.writeSampleData(mVideoTrackIndex, encodedData, mBufferInfo);
+            Log.i(TAG, "sent " + mBufferInfo.size + " bytes to muxer...");
+        }
+    }
+
+    private void resetOutputFormat() {
+        // should happen before receiving buffers, and should only happen once
+        if (mMuxerStarted) {
+            throw new IllegalStateException("output format already changed!");
+        }
+        MediaFormat newFormat = mEncoder.getOutputFormat();
+
+        Log.i(TAG, "output format changed.\n new format: " + newFormat.toString());
+        //通过 addTrack() 添加了数据通道之后，记录下函数返回的 trackIndex，然后就可以调用
+        //MediaMuxer.writeSampleData() 愉快地向mp4文件中写入数据了
+        mVideoTrackIndex = mMuxer.addTrack(newFormat);
+        mMuxer.start();
+        mMuxerStarted = true;
+        Log.i(TAG, "started media muxer, videoIndex=" + mVideoTrackIndex);
+    }
+
+
+
+    private void release() {
+        if (mEncoder != null) {
+            mEncoder.stop();
+            mEncoder.release();
+            mEncoder = null;
+        }
+        if (mVirtualDisplay != null) {
+            mVirtualDisplay.release();
+        }
+        if (mMediaProjection != null) {
+            mMediaProjection.stop();
+        }
+        if (mMuxer != null) {
+            mMuxer.stop();
+            mMuxer.release();
+            mMuxer = null;
+        }
+    }
+}
